14e906b2f8576ecb2be3913f17b436bc3f6fdc8f
mitetsu-kurone <mitetsu.kurone@gmail.com>
2020-10-26 09:55:28 +0800

    [added]first test

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/test",
+    name: "KuroneTest",
+    component: () =>
+      import(/* webpackChunkName: "test" */ "../views/KuroneTest.vue")
   }
 ];
 
---
+++ src/views/KuroneTest.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="kurone-test">
+    <h1>This is an about kurone test</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.kurone-test {
+  color: blue;
+}
+</style>
